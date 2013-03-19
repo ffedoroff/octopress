b2a85c40653fa14fcef78c5c6e2c17c19aa686fb
Roman Fedorov <rfedorov@linkentools.com>
2013-03-19 12:16:42 +0700

    add favico and convert.sh

--- source/audio/convert.sh
+++ source/audio/convert.sh
@@ -1,5 +1,12 @@
 # sudo apt-get install ffmpeg libavcodec-extra-53
+rm -f tmp.sh
 for f in *.m4a
-      do 
-      echo ffmpeg -i "\"$f\"" -b 192k "\"${f%.m4a}.mp3\""
+	do 
+	echo "rm -f t.mp3" >> tmp.sh
+	echo "avconv -i "\"$f\"" -map_metadata 0:s:0 -metadata comment=\"http://tinkov.linkentools.com/\" -b 80k -ac 1 t.mp3" >> tmp.sh
+	echo "vbrfix -always t.mp3 "\"${f%.m4a}.mp3\""" >> tmp.sh
+	echo "rm t.mp3" >> tmp.sh
 done
+echo "rm -f vbrfix* tmp.sh" >> tmp.sh
+#bash tmp.sh
+#echo "now run: bash tmp.sh"
